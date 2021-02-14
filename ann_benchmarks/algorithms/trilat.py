923982e316f2ca5bf9b33d0f7bbe7366c384a256
Chip Lynch <chip@chiplynch.com>
2021-02-13 20:25:37 -0800

    Mostly even more working trilat

--- ann_benchmarks/algorithms/trilat.py
+++ ann_benchmarks/algorithms/trilat.py
@@ -19,3 +19,20 @@ class Trilat(BaseANN):
             v = sklearn.preprocessing.normalize([v], axis=1, norm='l2')[0]
         dist, ind = self._tree.query([v], k=n)
         return ind[0]
+
+
+class TrilatApprox(BaseANN):
+    def __init__(self, metric):
+        self._metric = metric
+        self.name = 'TrilaterationApprox'
+
+    def fit(self, X):
+        if self._metric == 'angular':
+            X = sklearn.preprocessing.normalize(X, axis=1, norm='l2')
+        self._tree = sklearn.neighbors.TrilaterationIndex(X)
+
+    def query(self, v, n):
+        if self._metric == 'angular':
+            v = sklearn.preprocessing.normalize([v], axis=1, norm='l2')[0]
+        dist, ind = self._tree.query_expand([v], k=n)
+        return ind[0]
--- run_trilat.sh
+++ run_trilat.sh
@@ -1,4 +1,6 @@
 set -e
 # python run.py --definitions chip.yaml
-python run.py --local --algorithm trilat --run-disabled --dataset random-xs-20-euclidean --definitions chip.yaml
-python plot.py --dataset random-xs-20-euclidean
+# python run.py --local --algorithm ball --run-disabled --dataset random-xs-20-euclidean --definitions chip.yaml
+# python run.py --local --algorithm trilat --run-disabled --dataset random-xs-20-euclidean --definitions chip.yaml
+python run.py --local --algorithm trilatapprox --run-disabled --dataset random-xs-20-euclidean --definitions chip.yaml
+# python plot.py --dataset random-xs-20-euclidean
